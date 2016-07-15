86629aaeabb9de2797c6acc112d51ad3eac6535c
tracedeng <1401733434@qq.com>
2016-07-15 22:26:04 +0800

    创建 设备表 代理商表 订单表 维保表

--- market/models.py
+++ market/models.py
@@ -3,3 +3,33 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Appliances(models.Model):
+    identification = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.BigIntegerField()
+
+
+class Agent(models.Model):
+    name = models.CharField(max_length=128)
+    phone = models.CharField(max_length=32)
+    wechat = models.CharField(max_length=64)
+
+
+class Maintenance(models.Model):
+    name = models.CharField(max_length=128)
+    phone = models.CharField(max_length=32)
+    fix_address = models.CharField(max_length=32)
+    fix_date = models.DateField()
+    apply_time = models.TimeField()
+    handled = models.CharField(max_length=8)
+
+
+class Order(models.Model):
+    receipt_address = models.CharField(max_length=128)
+    receipt_date = models.DateField()
+    agent = models.ForeignKey(Agent)
+    order_time = models.TimeField()
+    payed = models.CharField(max_length=8)
+    order_index = models.CharField(max_length=64)
--- shuhe/settings.py
+++ shuhe/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'market',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'shuhe.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shuhe',
+        'USER': 'guest',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
