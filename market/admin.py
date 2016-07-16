e05c9290a4177ebfa3ea6da5630f98148539be5a
tracedeng <1401733434@qq.com>
2016-07-16 11:38:43 +0800

    组合bootstarp

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from market.models import Appliances, Agent, Maintenance, Order
+
+
+admin.site.register(Appliances)
+admin.site.register(Agent)
+admin.site.register(Maintenance)
+admin.site.register(Order)
