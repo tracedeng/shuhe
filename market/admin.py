fc681a2e9d52f6c6991dc6ed407e835b25671546
tracedeng <56997132@qq.com>
2016-07-17 17:59:53 +0800

    GE模型 管理页面展示列

--- market/admin.py
+++ market/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from market.models import Appliances, Agent, Maintenance, Order
+from market.models import Softener, Purifier,Drinking, Agent, Maintenance, Order
 
 
-class AppliancesAdmin(admin.ModelAdmin):
-    list_display = ('identification', 'description', 'price')
+class SoftenerAdmin(admin.ModelAdmin):
+    list_display = ('identification', 'description', 'price', 'outline_dimension', 'interface_size', 'service_flow', 'salt_tank_capacity', 'resin_quantity', 'resin_tank_size', 'regeneration_time', 'maximum_water_hardness', 'maximum_grains', 'maximum_iron_treatment')
+
+class PurifierAdmin(admin.ModelAdmin):
+    list_display = ('identification', 'description', 'price', 'outline_dimension', 'interface_size', 'filtering_accuracy', 'rated_flow', 'maximum_flow', 'water_pressure', 'water_temperature', 'cartridge_life')
+
+
+class DrinkingAdmin(admin.ModelAdmin):
+    list_display = ('identification', 'description', 'price', 'outline_dimension', 'interface_size', 'maximum_tds', 'maximum_flow', 'water_pressure', 'water_temperature', 'activated_carbon', 'ro_film')
 
 
 class AgentAdmin(admin.ModelAdmin):
@@ -21,7 +28,9 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
 
 
-admin.site.register(Appliances, AppliancesAdmin)
+admin.site.register(Softener, SoftenerAdmin)
+admin.site.register(Purifier, PurifierAdmin)
+admin.site.register(Drinking, DrinkingAdmin)
 admin.site.register(Agent, AgentAdmin)
 admin.site.register(Maintenance)
 admin.site.register(Order)
