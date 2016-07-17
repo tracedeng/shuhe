42cc63f12ab000cd7869d070a27512ba67c5b32c
tracedeng <1401733434@qq.com>
2016-07-17 16:27:43 +0800

    提交版本

--- shuhe/settings.py
+++ shuhe/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'market',
-    'books',
+    #'books',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'shuhe.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'shuhe',
-    #    'USER': 'guest',
-    #    'PASSWORD': '123456',
-    #    'HOST': '127.0.0.1',
-    #    'PORT': '3306',
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #}
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shuhe',
+        'USER': 'guest',
+        'PASSWORD': '123456',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
 
 
